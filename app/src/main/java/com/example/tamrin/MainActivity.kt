446c3f26ae12779595ac03383e5cfd597ce30641
mohammad <mpayvandian@gmail.com>
2024-02-17 21:55:06 +0330

    binding

--- app/src/main/java/com/example/tamrin/MainActivity.kt
+++ app/src/main/java/com/example/tamrin/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.tamrin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.tamrin.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
